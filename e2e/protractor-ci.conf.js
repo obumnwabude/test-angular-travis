1ae48daed28c0e415e67c830dc9e671a07ea3c1d
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-06-04 13:59:31 +0100

    Update protractor-ci.conf.js
    Remove 'we' that had no use and caused parse errors

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -2,7 +2,7 @@ const config = require('./protractor.conf').config;
 
 config.capabilities = {
   browserName: 'chrome',
-we  chromeOptions: {
+  chromeOptions: {
     args: ['--headless', '--no-sandbox']
   }
 };
